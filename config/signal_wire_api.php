c9bed5347bf336945e3dae0831a4a6ea9247c3b8
Abdul Basit <abdulbasit3398@gmail.com>
2021-10-28 19:19:42 +0500

    changes

--- config/signal_wire_api.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'signal_wire' => [
-        'project_id' => '8ce390f9-1f15-46ce-b97c-c87985a93a03',
-        'token'      => 'PTb6ff2005a26ddf5b0f2f74292ef1af7accb935cd9d6c614e',
-        'space_url'  => 'ecm.signalwire.com',
-    ],
-];
